2b375eb2525ca840e5bdb60e8f4131b2b0e0e577
kevin <1980817350@qq.com>
2022-07-02 16:37:39 +0800

    2022年7月2日16:37:32

---
+++ src/Medium/LC_513.java
@@ -0,0 +1,52 @@
+package Medium;
+
+import DataStructure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC_513 {
+
+    int max, ans;
+
+    public int findBottomLeftValue(TreeNode root) {  //层序遍历，思路是找到每次换层的第一个
+        Queue<TreeNode> queue = new LinkedList<>();
+        int ans = root.val, currentLen = 1, nextLen = 0;
+        queue.add(root);
+        while (currentLen > 0 || nextLen > 0) {
+            if (currentLen > 0) {
+                if (queue.peek().left != null) {
+                    queue.add(queue.peek().left);
+                    nextLen++;
+                }
+                if (queue.peek().right != null) {
+                    queue.add(queue.peek().right);
+                    nextLen++;
+                }
+                queue.poll();
+                currentLen--;
+            } else {
+                currentLen = nextLen;
+                nextLen = 0;
+                ans = queue.peek().val;
+            }
+        }
+        return ans;
+    }
+
+    public int findBottomLeftValue1(TreeNode root) { //深度优先搜索，优先遍历左子树，右子树只有当深度大于最大时才会被更新
+        dfs(root, 1);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int i) {
+        if (root != null) {
+            if (i > max) {
+                max = i;
+                ans = root.val;
+            }
+            dfs(root.left, i + 1);
+            dfs(root.right, i + 1);
+        }
+    }
+}
